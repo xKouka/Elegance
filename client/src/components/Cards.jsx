190d5e33cc0072fafa1312e97167bfdb25736bed
oscar <159394490+xKouka@users.noreply.github.com>
2025-04-03 01:29:51 -0400

    Update Cards.jsx

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -17,7 +17,7 @@ function Cards({tittle,price,img,id}){
         <div className="flex flex-col items-center justify-center">
         <Link
           to={`/producto/${id}`}
-          className="text-xl rounded-2xl w-24 h-11 border-2 justify-center border-black hover:bg-black hover:text-white m-2"
+          className="text-xl rounded-2xl w-24 h-11 border-2 flex flex-row justify-center items-center border-black hover:bg-black hover:text-white m-2"
         >
           Detalles
         </Link>
